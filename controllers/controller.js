5372684f20cdc934350457f9ddde7569360652ff
Athul PS <athulps336@gmail.com>
2024-05-01 10:25:33 +0530

    second commit

--- controllers/controller.js
+++ controllers/controller.js
@@ -48,5 +48,19 @@ const Login = async (req, res) => {
         return res.status(500).json({ error: err }); 
     }
 }
+//get user data
+const getUserDetails = async (req, res) => {
+    const { email } = req.params; 
 
-module.exports = { Register, Login };
+    try {
+        const user = await User.findOne({ email }); 
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        return res.status(200).json({ user: user });
+    } catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
+}
+
+module.exports = { Register, Login, getUserDetails };
--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { Register, Login } = require('../controllers/controller');
+const { Register, Login, getUserDetails } = require('../controllers/controller');
 
 router.post('/register', Register); 
 router.post('/login', Login); 
+router.get('/user/getUser/:email',getUserDetails);
 
 module.exports = router;
